65ebc37d57c430a4d096cb8361eae4fbb6443b11
Richard He <linyhe@microsoft.com>
2022-10-14 17:23:36 +0800

    Add: ArrayBuffer in mojom and C++ type

--- components/js_injection/common/web_message_mojom_traits.cc
+++ components/js_injection/common/web_message_mojom_traits.cc
@@ -5,9 +5,8 @@
 #include "components/js_injection/common/web_message_mojom_traits.h"
 
 #include <string>
+#include "base/notreached.h"
 #include "components/js_injection/common/interfaces.mojom.h"
-#include "components/js_injection/common/web_message.h"
-#include "mojo/public/cpp/bindings/union_traits.h"
 
 namespace mojo {
 
@@ -16,13 +15,23 @@ bool UnionTraits<js_injection::mojom::JsWebMessageDataView,
                  js_injection::JsWebMessage>::
     Read(js_injection::mojom::JsWebMessageDataView r,
          js_injection::JsWebMessage* out) {
-  std::u16string string_value;
-  if (!r.ReadStringValue(&string_value))
+  if (r.is_string_value()) {
+    std::u16string string_value;
+    if (!r.ReadStringValue(&string_value))
+      return false;
+    out->payload = std::move(string_value);
+    return true;
+  } else if (r.is_array_buffer_value()) {
+    mojo_base::BigBufferView array_buffer_view;
+    if (!r.ReadArrayBufferValue(&array_buffer_view))
+      return false;
+    out->payload = std::vector<uint8_t>(array_buffer_view.data().begin(),
+                                        array_buffer_view.data().end());
+    return true;
+  } else {
+    NOTREACHED() << "Unknown type for JsWebMessage mojo.";
     return false;
-
-  out->payload = std::move(string_value);
-
-  return true;
+  }
 }
 
 }  // namespace mojo
