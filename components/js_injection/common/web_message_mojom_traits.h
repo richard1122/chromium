50a28b06a7a195fa356ba2f81b5a13ffd5f58318
Richard He <linyhe@microsoft.com>
2022-10-14 17:23:36 +0800

    Add: ArrayBuffer in mojom and C++ type

--- components/js_injection/common/web_message_mojom_traits.h
+++ components/js_injection/common/web_message_mojom_traits.h
@@ -6,8 +6,10 @@
 #define COMPONENTS_JS_INJECTION_COMMON_WEB_MESSAGE_MOJOM_TRAITS_H_
 
 #include <string>
-#include "components/js_injection/common/interfaces.mojom-shared.h"
+#include "absl/types/variant.h"
+#include "components/js_injection/common/interfaces.mojom.h"
 #include "components/js_injection/common/web_message.h"
+#include "mojo/public/cpp/base/big_buffer.h"
 #include "mojo/public/cpp/bindings/union_traits.h"
 
 namespace mojo {
@@ -20,8 +22,19 @@ struct UnionTraits<js_injection::mojom::JsWebMessageDataView,
     return absl::get<std::u16string>(message.payload);
   }
 
+  static mojo_base::BigBuffer array_buffer_value(
+      const js_injection::JsWebMessage& message) {
+    return absl::get<std::vector<uint8_t>>(message.payload);
+  }
+
   static js_injection::mojom::JsWebMessageDataView::Tag GetTag(
       const js_injection::JsWebMessage& input) {
+    if (absl::holds_alternative<std::u16string>(input.payload)) {
+      return js_injection::mojom::JsWebMessageDataView::Tag::kStringValue;
+    } else if (absl::holds_alternative<std::vector<uint8_t>>(input.payload)) {
+      return js_injection::mojom::JsWebMessageDataView::Tag::kArrayBufferValue;
+    }
+    NOTREACHED() << "Unknown type for JsWebMessage.";
     return js_injection::mojom::JsWebMessageDataView::Tag::kStringValue;
   }
 
