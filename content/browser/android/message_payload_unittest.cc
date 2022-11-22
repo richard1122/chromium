00c3b71f3e10421311097f30c22c85d73e86cc14
Richard He <linyhe@microsoft.com>
2022-11-22 19:43:44 +0800

    Refactor ArrayBuffer in WebMessagePayload to a pure interface
    This CL refactors the ArrayBuffer message payload type in
    Browser process, from Vector to an interface:
    `WebMessageArrayBufferPayload`.
    Previously Java and mojo::TransferableMessage conversion requires
    to create a vector, which is necessary. Now Java ByteArray can be
    directly copied to and from mojo types.
    
    The existing CL to add ArrayBuffer support for js_injection post
    message API will adopt this new type as well:
    https://chromium-review.googlesource.com/c/chromium/src/+/3970281
    
    Bug: 1382405
    Change-Id: I1aecdbd422482b574883e6eccd54d16580fc270c

--- content/browser/android/message_payload_unittest.cc
+++ content/browser/android/message_payload_unittest.cc
@@ -24,15 +24,37 @@ TEST(MessagePayloadTest, SelfTest_String) {
 TEST(MessagePayloadTest, SelfTest_ArrayBuffer) {
   std::vector<uint8_t> data(200, 0XFF);
   auto generated_message = android::ConvertToWebMessagePayloadFromJava(
-      android::ConvertWebMessagePayloadToJava(data));
-  EXPECT_EQ(blink::WebMessagePayload(data), generated_message);
+      android::ConvertWebMessagePayloadToJava(
+          blink::WebMessageArrayBufferPayload::CreateForTesting(data)));
+  const auto& array_buffer =
+      absl::get<blink::WebMessageArrayBufferPayloadPtr>(generated_message);
+  EXPECT_EQ(data.size(), array_buffer->GetLength());
+  EXPECT_FALSE(array_buffer->SupportGetArrayBuffer());
+  std::vector<uint8_t> copied_data(data.size());
+  array_buffer->CopyArrayBufferInto(base::make_span(copied_data));
+  EXPECT_EQ(data, copied_data);
+
+  // Encode the message and decode it again. This time the ArrayBuffer should be
+  // stored in Java ByteArray, which does not support |GetArrayBuffer|.
+  auto generated_message2 = android::ConvertToWebMessagePayloadFromJava(
+      android::ConvertWebMessagePayloadToJava(generated_message));
+  const auto& array_buffer2 =
+      absl::get<blink::WebMessageArrayBufferPayloadPtr>(generated_message2);
+  EXPECT_EQ(data.size(), array_buffer2->GetLength());
+  copied_data.clear();
+  copied_data.resize(data.size());
+  array_buffer->CopyArrayBufferInto(base::make_span(copied_data));
+  EXPECT_EQ(data, copied_data);
 }
 
 TEST(MessagePayloadTest, SelfTest_ArrayBufferEmpty) {
-  std::vector<uint8_t> data;
   auto generated_message = android::ConvertToWebMessagePayloadFromJava(
-      android::ConvertWebMessagePayloadToJava(data));
-  EXPECT_EQ(blink::WebMessagePayload(data), generated_message);
+      android::ConvertWebMessagePayloadToJava(
+          blink::WebMessageArrayBufferPayload::CreateForTesting(
+              std::vector<uint8_t>())));
+  EXPECT_EQ(absl::get<blink::WebMessageArrayBufferPayloadPtr>(generated_message)
+                ->GetLength(),
+            0u);
 }
 
 }  // namespace
